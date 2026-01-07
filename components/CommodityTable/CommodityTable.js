d49dc8228d429ed8557aa8566056547e0b546a10
Amaljith <amaljithaurify@gmail.com>
2026-01-07 16:44:24 +0530

    Video Component Creted

--- components/CommodityTable/CommodityTable.js
+++ components/CommodityTable/CommodityTable.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./CommodityTable.module.scss";
+import Image from "next/image";
 
 const CommodityTable = ({
     commodities,
@@ -12,14 +13,16 @@ const CommodityTable = ({
             <li>
                 <div className={styles.title_sec}>
                     <h3>Commodity</h3>
-                    <h3>BID</h3>
-                    <h3>ASK</h3>
+                    <h3>Unit</h3>
+                    <h3>BID ( <Image src={'./icons/dirham-icon.svg'} height={300} width={300} alt="" />)</h3>
+                    <h3>ASK ( <Image src={'./icons/dirham-icon.svg'} height={300} width={300} alt="" />)</h3>
                 </div>
             </li>
 
             {commodities.map((item, index) => {
                 const metalName = getMetalName(item.metal);
                 const { bidPrice, askPrice } = calculatePrices(item, metalName);
+                console.log('adadasdasd', item);
 
                 return (
                     <li key={index} className={styles.detail_content_sec}>
@@ -27,6 +30,7 @@ const CommodityTable = ({
                             <span>
                                 {item.purity} {metalName}
                             </span>
+                            <span>{item.unit}{item.weight}</span>
                             <span>{currencyFormatter.format(bidPrice)}</span>
                             <span>{currencyFormatter.format(askPrice)}</span>
                         </div>
